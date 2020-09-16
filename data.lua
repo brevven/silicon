ef39927bbdeb07ba32d20d77babf35e38e4cf136
Brevven <brevingame@gmail.com>
2020-09-16 00:04:58 -0700

    initial optical fiber stuff

--- data.lua
+++ data.lua
@@ -1,3 +1,4 @@
 require("prototypes/silica-recipe")
 require("prototypes/silicon-recipe")
+require("prototypes/optical-fiber")
 
---
+++ prototypes/optical-fiber.lua
@@ -0,0 +1,61 @@
+-- Optical Fiber
+data:extend(
+{
+  {
+    type = "item",
+    name = "optical-fiber",
+    icon = "__bzsilicon__/graphics/icons/optical-fiber.png",
+    icon_size = 32,
+    subgroup = "intermediate-product",
+    order = "a[optical-fiber]",
+    stack_size = 200
+  },
+  {
+    type = "recipe",
+    name = "optical-fiber",
+    normal = 
+    {
+      enabled = false,
+      ingredients = {{"silica", 1}},
+      result = "optical-fiber",
+      result_count = 1
+    },
+    expensive =
+    {
+      enabled = false,
+      ingredients = {{"silica", 2}},
+      result = "optical-fiber",
+      result_count = 1
+    },
+  },
+
+  {
+    type = "technology",
+    name = "fiber-optics",
+    icons =
+    {
+      { icon = "__base__/graphics/technology/optics.png", icon_size = 128 },
+      { icon = "__bzsilicon__/graphics/icons/optical-fiber.png", icon_size = 32, scale=1.5, shift={32,-48} },
+    },
+    effects =
+    {
+      {
+        type = "unlock-recipe",
+        recipe = "optical-fiber"
+      }
+    },
+    unit =
+    {
+      count = 100,
+      ingredients =
+      {
+        {"automation-science-pack", 1},
+        {"logistic-science-pack", 1},
+      },
+      time = 10
+    },
+    prerequisites = {"optics", "silica-processing"},
+    order = "b-b"
+  },
+}
+)
