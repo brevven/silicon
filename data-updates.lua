b42b47662291bf3c0b11f9b2daa9cb95255af864
Brevven <brevingame@gmail.com>
2021-12-31 04:19:40 -0800

    stone autoplace

--- data-updates.lua
+++ data-updates.lua
@@ -2,11 +2,23 @@ require("recipe-updates")
 require("strange-matter")
 require("recipes/silicon-vulcanite")
 
-
 if data.raw.resource["stone"] then
-  local noise = require('noise');
-  -- Increase richness of stone a bit
-  data.raw.resource["stone"].autoplace.richness_expression = 
-    data.raw.resource["stone"].autoplace.richness_expression * noise.to_noise_expression(4/3)
+  if mods["space-exploration"] then
+    local noise = require('noise');
+    -- Increase richness of stone a bit
+    data.raw.resource["stone"].autoplace.richness_expression = 
+      data.raw.resource["stone"].autoplace.richness_expression * noise.to_noise_expression(4/3)
+  else
+    log("Replacing vanilla stone autoplace")
+    local resource_autoplace = require('resource-autoplace');
+    data.raw.resource["stone"].autoplace = resource_autoplace.resource_autoplace_settings{
+      name = "stone",
+      order = "b",
+      base_density = 7, -- increased from 4 in vanilla
+      has_starting_area_placement = true,
+      regular_rq_factor_multiplier = 1.0,
+      starting_rq_factor_multiplier = 1.1,
+    }
+  end
 end
     
