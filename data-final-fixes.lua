7d98c68ecc5edace856ec1edc2868e440dfdd452
Brevven <brevingame@gmail.com>
2023-01-01 01:05:29 -0800

    locomotive gyro

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -19,6 +19,10 @@ util.add_to_product("vtk-deepcore-mining-ore-chunk-refining-no-uranium", "vtk-de
 util.set_vtk_dcm_ingredients()
 
 if util.me.use_gyros() then
+  util.add_ingredient("recipe-space-locomotive", "gyro", 1) -- must be in final fixes
+end
+
+if util.me.use_gyros() then
   -- Mod portal reported issue, can't reproduce, attempt to add gyro at the very end
   util.add_ingredient("flying-robot-frame", "gyro", 1)
 end
