13f1834a55f4fe82cfcc754a6a3051b2c9ffb0e5
Brevven <brevingame@gmail.com>
2024-01-13 13:39:32 -0800

    matter update

--- recipes/silica-matter.lua
+++ recipes/silica-matter.lua
@@ -1,14 +1,14 @@
 if mods["Krastorio2"] then
-local matter = require("__Krastorio2__/lib/public/data-stages/matter-util")
-local silica_matter = 
-  {
-    item_name = "silica",
-    minimum_conversion_quantity = 10,
-    matter_value = .65,
-    energy_required = 2,
-    only_deconversion = false,
-    need_stabilizer = false,
-    unlocked_by_technology = "kr-matter-stone-processing"
-  }
-matter.createMatterRecipe(silica_matter)
+  local util = require("data-util");
+  util.k2matter({
+    k2matter = {
+      item_name = "silica",
+      minimum_conversion_quantity = 20,
+      matter_value = .65,
+      energy_required = 2,
+      only_deconversion = false,
+      need_stabilizer = false,
+      unlocked_by_technology = "kr-matter-stone-processing"
+    },
+  })
 end
